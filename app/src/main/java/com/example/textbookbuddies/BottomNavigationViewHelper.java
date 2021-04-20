92e7f8e7369ae878e34d0784cc00db6dc8873ad4
emilyrn18 <emilyrn18@vt.edu>
2021-04-20 19:31:47 -0400

    I created icon drawables and added the navbar component on homepage.

---
+++ app/src/main/java/com/example/textbookbuddies/BottomNavigationViewHelper.java
@@ -0,0 +1,35 @@
+package com.example.textbookbuddies;
+
+//This code is from https://github.com/mitchtabian/AppBarLayouts/blob/master/ActionBar%20Bottom%20Navigation/ActionBar/app/src/main/java/tabian/com/actionbar/BottomNavigationViewHelper.java
+//With some adjustments
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.util.Log;
+import java.lang.reflect.Field;
+
+public class BottomNavigationViewHelper {
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShifting(false);
+//                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+}
--- app/src/main/java/com/example/textbookbuddies/HomeActivity.java
+++ app/src/main/java/com/example/textbookbuddies/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.textbookbuddies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+//
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch(item.getItemId()):
+//                case R.id.ic_arrow:
+//                case R.id.ic_search:
+//                case R.id.ic_home:
+//                case R.id.ic_listings:
+//                case R.id.ic_help:
+//
+//                return false;
+//            }
+//        });
     }
 }
