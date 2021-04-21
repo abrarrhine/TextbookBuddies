07d00086a59dbaccd22a1b32c6e1e0224a42bd6c
emilyrn18 <emilyrn18@vt.edu>
2021-04-21 00:34:04 -0400

    Added Nav bar to all pages

--- app/src/main/java/com/example/textbookbuddies/EditProfile.java
+++ app/src/main/java/com/example/textbookbuddies/EditProfile.java
@@ -1,8 +1,13 @@
 package com.example.textbookbuddies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.ic_search:
+                        Intent intent1 = new Intent(EditProfile.this, Search.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.ic_home:
+                        Intent intent2 = new Intent(EditProfile.this, HomeActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.ic_listings:
+                        Intent intent3 = new Intent(EditProfile.this, Listings.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.ic_help:
+                        Intent intent4 = new Intent(EditProfile.this, FAQ.class);
+                        startActivity(intent4);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
