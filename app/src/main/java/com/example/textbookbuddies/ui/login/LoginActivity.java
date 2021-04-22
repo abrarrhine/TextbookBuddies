6ec31d498b0f8bd3d5640ff10e548e24789964d4
emilyrn18 <emilyrn18@vt.edu>
2021-04-21 21:43:51 -0400

    Added login and signup capabilities.

--- app/src/main/java/com/example/textbookbuddies/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/textbookbuddies/ui/login/LoginActivity.java
@@ -6,12 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Parcel;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -24,7 +26,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.textbookbuddies.HomeActivity;
 import com.example.textbookbuddies.R;
+import com.example.textbookbuddies.Signup;
 import com.example.textbookbuddies.ui.login.LoginViewModel;
 import com.example.textbookbuddies.ui.login.LoginViewModelFactory;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -32,6 +36,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.parceler.Parcels;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -54,7 +60,6 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-
         et_username = (EditText) findViewById(R.id.et_username);
         et_password = (EditText) findViewById(R.id.et_password);
 
@@ -65,29 +70,20 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
-        });
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
                 email = et_username.getText().toString();
                 password = et_password.getText().toString();
-                Log.d("Email: ", email);
-                Log.d("Pass: ", password);
-                mAuth.createUserWithEmailAndPassword(email, password)
+                mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
+                                    Log.d(TAG, "signInWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
                                     updateUI(user);
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
                                     Toast.makeText(LoginActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                     updateUI(null);
@@ -96,17 +92,21 @@ public class LoginActivity extends AppCompatActivity {
                         });
             }
         });
-
-
-
-
-
-
-
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent = new Intent(LoginActivity.this, Signup.class);
+                startActivity(signupIntent);
+            }
+        });
     }
 
     private void updateUI(Object o) {
-
+        if(o == mAuth.getCurrentUser()){
+            Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+            i.putExtra("userobject", Parcels.wrap(o));
+            startActivity(i);
+        }
     }
 
 
