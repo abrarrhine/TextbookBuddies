abe59ecc2538ca4c2a90c1c6217b191fb89453ef
tranthai <tranthai@vt.edu>
2021-04-25 02:31:55 -0400

    Edit colors for actionbar for homepage

--- app/src/main/java/com/example/textbookbuddies/HomeActivity.java
+++ app/src/main/java/com/example/textbookbuddies/HomeActivity.java
@@ -2,22 +2,37 @@ package com.example.textbookbuddies;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Headers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.textbookbuddies.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageView iv_logout;
-    TextView logout;
+    String userId;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        //BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
 
             @Override
@@ -54,28 +69,47 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        iv_logout = (ImageView) findViewById(R.id.iv_logout);
-        logout = (TextView) findViewById(R.id.tv_logout);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        mDatabase.child("users").child(userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onClick(View view) {
-                Intent intent5 = new Intent(HomeActivity.this, LoginActivity.class);
-                FirebaseAuth.getInstance().signOut();
-                startActivity(intent5);
-
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
             }
         });
 
-        iv_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent6 = new Intent(HomeActivity.this, Profile.class);
-                startActivity(intent6);
+    }
 
-            }
-        });
+    // create an action bar button
+    //@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == R.id.action_logout) {
+            // do something here
+            Intent intent5 = new Intent(HomeActivity.this, LoginActivity.class);
+            FirebaseAuth.getInstance().signOut();
+            startActivity(intent5);
+        }
+        else if (id == R.id.action_profile){
+            Intent intent6 = new Intent(HomeActivity.this, Profile.class);
+            startActivity(intent6);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
