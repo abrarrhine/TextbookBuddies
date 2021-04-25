00289fb7f783c9778b8abbdafb87438d462aaa84
emilyrn18 <emilyrn18@vt.edu>
2021-04-24 21:33:46 -0400

    Figured out how to retrieve user data using Http Async Requests to Firebase in book listings

--- app/src/main/java/com/example/textbookbuddies/User.java
+++ app/src/main/java/com/example/textbookbuddies/User.java
@@ -10,13 +10,13 @@ public class User {
     public User(){
 
     }
-    public User(String firstname, String lastname, String dateOfBirth, String email, String phonenumber){
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.dateOfBirth = dateOfBirth;
-        this.email = email;
-        this.phonenumber = phonenumber;
-    }
+//    public User(String firstname, String lastname, String dateOfBirth, String email, String phonenumber){
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.dateOfBirth = dateOfBirth;
+//        this.email = email;
+//        this.phonenumber = phonenumber;
+//    }
 
     public User(String firstname, String lastname, String dateOfBirth, String email, String phonenumber,ArrayList<Book> booklist){
         this.firstname = firstname;
@@ -27,6 +27,51 @@ public class User {
         this.booklist = booklist;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public ArrayList<Book> getBooklist() {
+        return booklist;
+    }
+
+    public void setBooklist(ArrayList<Book> booklist) {
+        this.booklist = booklist;
+    }
 }
